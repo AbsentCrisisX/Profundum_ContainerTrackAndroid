56ee261be6f59511d767f5142d6cbfba2b923253
J. Kraaijkamp <0866265@hr.nl>
2014-05-06 09:37:50 +0200

    First commit

---
+++ CointainerTracker/gen/nl/acxdev/profundum/cointainertracker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nl.acxdev.profundum.cointainertracker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
