dfc59044774beacbeff6cf86cbc647bd9d86861b
J. Kraaijkamp <0866265@hr.nl>
2014-05-12 16:52:56 +0200

    Added Splash screen, Dummy login screen, Dummy scanner screen

--- src/nl/acxdev/profundum/containertracker/MainActivity.java
+++ src/nl/acxdev/profundum/containertracker/MainActivity.java
@@ -1,15 +1,55 @@
 package nl.acxdev.profundum.containertracker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	EditText usr;
+	EditText pwd;
+	Button sig;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		usr = (EditText) findViewById(R.id.usr);
+		pwd = (EditText) findViewById(R.id.password);
+		sig = (Button) findViewById(R.id.signIn);
+		
+		sig.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click   
+            	            	        	
+                String usrS = (String) usr.getText().toString();
+                String pwdS = (String) pwd.getText().toString();
+                
+                if(usrS.equals("admin") && pwdS.equals("admin")){
+                	Context context = getApplicationContext();
+                	CharSequence text = "Login succeeded!";
+                	int duration = Toast.LENGTH_LONG;
+                	
+                	Toast.makeText(context, text, duration).show();
+                	
+                	Intent myIntent = new Intent(MainActivity.this, ScanEdit.class);
+                	MainActivity.this.startActivity(myIntent);
+                } else {
+                	Context context = getApplicationContext();
+                	CharSequence text = "Username or password is incorrect";
+                	int duration = Toast.LENGTH_LONG;
+                	
+                	Toast.makeText(context, text, duration).show();
+                }
+            }
+        });
 	}
 
 	@Override
