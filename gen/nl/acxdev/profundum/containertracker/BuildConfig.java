6c3b3cfb91226eff1e13a2aa9ceaeb8448de0ada
J. Kraaijkamp <0866265@hr.nl>
2014-05-12 13:22:13 +0200

    revamp of first commit

---
+++ gen/nl/acxdev/profundum/containertracker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nl.acxdev.profundum.containertracker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
